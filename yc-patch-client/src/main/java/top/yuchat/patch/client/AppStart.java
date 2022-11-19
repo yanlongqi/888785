b3d7087cb7bc42be6c73050df43d166cc344b27b
燕陇琪 <1037120743@qq.com>
2022-11-19 10:57:02 +0800

    2.0.0项目初始化

---
+++ yc-patch-client/src/main/java/top/yuchat/patch/client/AppStart.java
@@ -0,0 +1,8 @@
+package top.yuchat.patch.client;
+
+public class AppStart {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ yc-patch-server/src/main/java/top/yuchat/patch/server/YcPatchServerApplication.java
@@ -0,0 +1,12 @@
+package top.yuchat.patch.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcPatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcPatchServerApplication.class, args);
+    }
+}
